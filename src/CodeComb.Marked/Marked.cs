208109a236c366d31d169524f8feba7db51cc990
あまみや ゆうこ <1@1234.sh>
2015-10-08 11:06:44 +0800

    Initialize

---
+++ src/CodeComb.Marked/Marked.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CodeComb.Marked
+{
+    public static class Marked
+    {
+        private static _Marked marked = new _Marked();
+        public static string Parse(string MarkdownContent)
+        {
+            return marked.Parse(MarkdownContent);
+        }
+    }
+
+    internal class _Marked
+    {
+        public Options Options { get; set; }
+        
+        public _Marked()
+            : this(null)
+        {
+        }
+
+        public _Marked(Options options)
+        {
+            Options = options ?? new Options();
+        }
+
+
+        public virtual string Parse(string src)
+        {
+            if (String.IsNullOrEmpty(src))
+            {
+                return src;
+            }
+
+            var tokens = Lexer.Lex(src, Options);
+            var result = Parser.Parse(tokens, Options);
+            return result;
+        }
+    }
+}
